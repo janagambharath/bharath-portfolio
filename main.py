f457c4f21adf190409a3e8d84ba53f0e2d1ecb0f
janagambharath <90457142+janagambharath@users.noreply.github.com>
2025-07-27 22:53:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from flask import Flask, render_template, send_from_directory
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/resume")
+def resume():
+    return send_from_directory(".", "resume.pdf")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
