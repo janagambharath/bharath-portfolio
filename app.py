70df58653a6d11e6f989d902577f3e80eb647bfa
janagambharath <90457142+janagambharath@users.noreply.github.com>
2025-07-28 17:15:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -92,4 +92,5 @@ def get_contact():
     return jsonify(contact_info)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
