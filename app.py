2328654cce61cda5cf88eb61ef20fc231e3b80c6
janagambharath <90457142+janagambharath@users.noreply.github.com>
2025-08-31 18:03:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ portfolio_data = {
     
     'projects': [
         {
-            'name':'Bharath's Ai',
+            'name':'Bharath Ai',
             'language':'Python',
             'description':'It is Bharath's personal portfolio assistance and Ai',
             'tech_stack':['Python','Flask','Deepsake Ai','Render','Json','Api']
